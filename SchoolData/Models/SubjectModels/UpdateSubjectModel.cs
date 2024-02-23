e5a40cdff7dc294a4f24627ea6f04e2e1c4e77c6
fuga002 <159753999+fuga002@users.noreply.github.com>
2024-02-23 10:52:18 +0500

    ok

--- SchoolData/Models/SubjectModels/UpdateSubjectModel.cs
+++ SchoolData/Models/SubjectModels/UpdateSubjectModel.cs
@@ -5,6 +5,8 @@ namespace SchoolData.Models.SubjectModels;
 public class UpdateSubjectModel
 {
     [Required]
+    public int SubjectId { get; set; }
+    [Required]
     public string SubjectName { get; set; }
     [Required]
     public string SubjectDescription { get; set; }
